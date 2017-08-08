57473a68b4dd20ed8d35fdeb9c8b156e212c95c1
ankit.singl <ankit.singl@MMMDVANKITSINGL.local>
2017-08-08 11:54:40 +0530

    Capturing details

--- myJs.js
+++ myJs.js
@@ -1,3 +1,10 @@
 function chooseFile() {
       $("#fileupload").click();
    }
+
+function applyFilters() {
+	var ageLowerBound=document.getElementById('ageLowerBound').value;
+	var ageUpperBound=document.getElementById('ageUpperBound').value;
+	var gender = document.querySelector('input[name = "gender"]:checked').value;
+	alert(gender);
+}
